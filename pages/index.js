e703fbd8206a8b74a5605ad2de6618ec5d98e7c3
Jonathan <jonbezeau@gmail.com>
2019-08-27 15:12:56 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = () => (
+    <div>
+        <h1>Portfolio Website</h1>
+        <a href="https://ultravioletvoodoo.github.io/Character_Generator/" target="_blank">Character Generator</a>
+    </div>
+);
+
+export default Index;
