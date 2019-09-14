6635aefd57fe6d67028e62638b1e6a5d577f4aa7
Jonathan <jonbezeau@gmail.com>
2019-09-13 20:57:00 -0700

    Improved navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,14 +1,16 @@
 import MiniLogo from "./miniLogo";
+import "./navbar.css";
 
 export default function Navbar() {
     return (
-        <header className="navbar">
-            <section className="navbar-section">
-                <MiniLogo></MiniLogo>
-            </section>
-            <section className="navbar-center"></section>
+        <header className="navbar myNavbar">
             <section className="navbar-section">
                 <a className="btn btn-link">About</a>
+            </section>
+            <section className="navbar-center">
+                <MiniLogo></MiniLogo>
+            </section>
+            <section className="navbar-section">
                 <a className="btn btn-link">Resume</a>
             </section>
         </header>
