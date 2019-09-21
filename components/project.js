e4d8be5677e2cb700a1265fc6e1b9997ef548bff
Jonathan <jonbezeau@gmail.com>
2019-09-21 12:37:48 -0700

    Projects progress

--- components/project.js
+++ components/project.js
@@ -1,16 +1,37 @@
-export default function Project() {
+import "./project.css";
+
+export default function Project(props) {
+    
+    function checkImg() {
+        if (props.imgSrc == null) return "static/img/placeHolder.png";
+        return props.imgSrc;
+    }
+
+    function checkBtnClasses() {
+        if(props.btnLink == null) return "btn disabled";
+        return "btn";
+    }
+
     return (
         <div className="col-3 col-lg-4 col-md-6 col-sm-12 col-mx-auto">
-            <div className="card">
+            <div className="card myCard">
                 <div className="card-image">
-                    <img src="/static/img/placeholder.png" className="img-responsive"></img>
+                    <img src={checkImg()} className="img-responsive"></img>
                 </div>
                 <div className="card-header">
-                    <div className="card-title h5">Project Name</div>
+                    <div className="card-title h5">{props.name}</div>
                 </div>
                 <div className="card-body">
-                    <p>Project description...</p>
-                    <button className="btn">Visit Site</button>
+                    <p>{props.desc}</p>
+                    <div className="card-button">
+                        <a 
+                            className={checkBtnClasses()}
+                            href={props.btnLink}
+                            target="_blank"
+                            >
+                            Visit Site
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
