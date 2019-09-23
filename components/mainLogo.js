54dddbceab1d36b8efd9f07c4cd10404365af838
Jonathan <jonbezeau@gmail.com>
2019-09-22 17:42:27 -0700

    greatly improved the splash screen css

--- components/mainLogo.js
+++ components/mainLogo.js
@@ -3,13 +3,20 @@ import MiniAbout from "../components/miniAbout";
 
 export default function MainLogo() {
     return (
-        <div className="columns">
-            <div className="col-12">
-                <div className="logoAboutContainer">
-                    <img src="/static/img/treasure.svg" className="mainLogo"></img>
-                    <MiniAbout></MiniAbout>
-                </div>
-            </div>
+        // <div className="columns">
+        //     <div className="col-12">
+        //         <div className="logoAboutContainer">
+        //             <img src="/static/img/treasure.svg" className="mainLogo"></img>
+        //             <MiniAbout></MiniAbout>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="logoAboutContainer">
+            <img src="/static/img/treasure.svg" className="mainLogo"></img>
+            <h3 className="myName">Jonathan Bezeau</h3>
+            <MiniAbout></MiniAbout>
         </div>
+        
     );
 }
