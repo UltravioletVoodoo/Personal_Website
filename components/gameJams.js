cf0fe2b7c344d7a1bf04cf2aeb1a1a81a49d2d06
Jonathan <jonbezeau@gmail.com>
2021-10-24 05:18:55 -0700

    Added Requiem of the Rift to game jams

--- components/gameJams.js
+++ components/gameJams.js
@@ -28,6 +28,14 @@ export default function GameJams() {
                     >
                 </Project>
 
+                <Project
+                    name="Requiem of the Rift"
+                    desc="A spooky space rhythm game. Created in 48 hours on a 5 person team in October of 2021."
+                    btnLink="https://emanaretne.itch.io/requiem-of-the-rift"
+                    imgSrc="/static/img/RequiemOfTheRift.png"
+                    >
+                </Project>
+
             </div>
         </>
     );
