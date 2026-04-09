db918134963a6602e20a2fb4a273744773bd4178
UltravioletVoodoo <jonbezeau@gmail.com>
2026-04-08 23:46:30 -0700

    Enhance projects

--- components/project.js
+++ components/project.js
@@ -1,27 +1,25 @@
+import { useCallback } from "react";
 import style from "../styles/project.module.css";
 
-export default function Project(props) {
+export function Project(props) {
   const { btnLink, name, desc, imgSrc } = props;
 
+  const navigate = useCallback(() => {
+    window.open(btnLink, "_blank");
+  }, [btnLink]);
+
   return (
-    <div>
-      <div className={style.myCard}>
-        <a href={btnLink} target="_blank">
-          <img
-            src={imgSrc ?? "/static/img/placeholder.png"}
-            className={style.cardImage}
-          ></img>
-        </a>
-        <div>
-          <div>{name}</div>
-        </div>
-        <div>
-          <p>{desc}</p>
-          <div>
-            <a href={btnLink} target="_blank">
-              Visit Site
-            </a>
-          </div>
+    <div className={style.project}>
+      <a href={btnLink} target="_blank">
+        <img src={imgSrc} className={style.cardImage}></img>
+      </a>
+      <div className={style.content}>
+        <h3 className={style.title}>{name}</h3>
+        <p>{desc}</p>
+        <div className={style.projectButtonContainer}>
+          <button className={style.projectButton} onClick={navigate}>
+            Visit Site
+          </button>
         </div>
       </div>
     </div>
---
+++ components/projectContainer.js
@@ -0,0 +1,22 @@
+import { Project } from "./project";
+import style from "../styles/projectContainer.module.css";
+
+export function ProjectContainer(props) {
+  const { title, projects } = props;
+
+  return (
+    <div className={style.projectContainer}>
+      <h2>{title}</h2>
+      <div className={style.projects}>
+        {projects.map((project) => (
+          <Project
+            name={project.name}
+            desc={project.description}
+            btnLink={project.href}
+            imgSrc={project.src}
+          ></Project>
+        ))}
+      </div>
+    </div>
+  );
+}
